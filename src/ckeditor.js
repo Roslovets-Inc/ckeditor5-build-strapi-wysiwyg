4b81a974d89be415b1f4ecf2b8a16adffb194f92
Pavel Roslovets <roslovets@exponenta.ru>
2021-02-04 14:14:38 +0300

    Add Header 4 (h4)

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -144,6 +144,15 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+	heading: {
+		options: [
+			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+			{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+			{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' }
+		]
+	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
