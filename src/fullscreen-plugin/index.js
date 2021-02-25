fd81e7d914924a993fb83a52fba289076a0c8ede
Pavel Roslovets <roslovets@exponenta.ru>
2021-02-26 01:43:51 +0300

    Prettify fullscreen-plugin code

--- src/fullscreen-plugin/index.js
+++ src/fullscreen-plugin/index.js
@@ -5,43 +5,48 @@ import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import './css/style.css';
 
 export default class FullScreen extends Plugin {
+
+    static get pluginName() {
+        return 'fullScreen'
+    }
+
     init() {
         const editor = this.editor;
 
-        editor.ui.componentFactory.add( 'fullScreen', locale => {
-            const view = new ButtonView( locale );
-            let etat = 0; //si 0 position normale
-            view.set( {
-                label: 'Full Screen',
-                icon: ImageFullBig,
-                tooltip: true
-            } );
+        editor.ui.componentFactory.add('fullScreen', locale => {
 
-            // Callback executed once the image is clicked.
-            view.on( 'execute', () => {
-                if(etat==1){
+            const view = new ButtonView(locale);
+
+            let normalButtonState = true; // button tooltip suggests to toggle full screen;
+
+            const updateButtonState = function () {
+                if (normalButtonState) {
                     editor.sourceElement.nextElementSibling.removeAttribute('id');
                     document.body.removeAttribute('id');
-                    view.set( {
-                        label: 'Full Screen',
+                    view.set({
+                        label: 'Full screen',
                         icon: ImageFullBig,
                         tooltip: true
-                    } );
-                    etat=0;
-                }else{
-                    editor.sourceElement.nextElementSibling.setAttribute("id", 'fullscreeneditor');
-                    document.body.setAttribute("id", "fullscreenoverlay");
-                    view.set( {
-                        label: 'Normal Mode',
+                    });
+                    normalButtonState = false;
+                } else {
+                    editor.sourceElement.nextElementSibling.setAttribute('id', 'fullscreeneditor');
+                    document.body.setAttribute('id', 'fullscreenoverlay');
+                    view.set({
+                        label: 'Normal view',
                         icon: ImageFullCancel,
                         tooltip: true
-                    } );
-                    etat=1;
+                    });
+                    normalButtonState = true;
                 }
-            
-            } );
+            }
+
+            updateButtonState();
+
+            // Callback executed once the image is clicked
+            view.on('execute', updateButtonState);
 
             return view;
-        } );
+        });
     }
 }
